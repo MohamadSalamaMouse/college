45eec1c4bf5d4dc04134b740953fed09eed59adf
MohamedSalamaMouse <slamtm608@gmail.com>
2023-02-02 22:25:49 +0200

    skills

--- admin/backend/student.php
+++ admin/backend/student.php
@@ -3,5 +3,5 @@ require_once "connection.php";
 $q="SELECT concat (fname,' ', lname)as 'name',address,gender,code,name as 'department' FROM students LEFT JOIN departments ON students.dept_num=departments.number";
 $result=$conect->query($q);
 $students=$result->fetchAll(PDO::FETCH_ASSOC);
-
-?>
+$s = $conect->query("SELECT *FROM skills");
+$skills = $s->fetchAll(PDO::FETCH_ASSOC);
